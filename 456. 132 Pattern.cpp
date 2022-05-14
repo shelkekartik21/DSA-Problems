62b254b2443fae88f8b2749ebe6f9abcc501a6b5
shelkekartik21 <shelkekartik21@gmail.com>
2022-05-14 14:05:23 +0530

    LeetCode 456

---
+++ 456. 132 Pattern.cpp
@@ -0,0 +1,17 @@
+ bool find132pattern(vector<int>& nums) {
+        int min = INT_MIN;
+        stack<int> st;
+        
+        for(int i=nums.size()-1; i>=0; i--){
+            if(nums[i] < min) return true;
+            else{
+                while(!st.empty() && nums[i] > st.top()){
+                    min = st.top();
+                    st.pop();
+                }
+            }
+            st.push(nums[i]);
+        }
+        
+        return false;
+    }
